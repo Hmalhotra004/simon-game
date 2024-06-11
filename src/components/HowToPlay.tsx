2202d25c1b14b86802c98e7f06a6539cfa641225
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-11 19:41:03 +0530

    w

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -1,5 +1,6 @@
 import { SetStateAction, forwardRef, useRef } from "react";
 import styles from "./howtoplay.module.scss";
+import Reveal from "./Reveal";
 
 const LIST = [{ li: `Simon will give the first signal. Repeat the signal by pressing the same color lens.` }, { li: `Simon will add another signal. Repeat these two signals by pressing the same color lenses, in order.` }, { li: `Continue playing as long as you can repeat each of signal correctly.` }];
 
@@ -22,30 +23,45 @@ const HowToPlay = forwardRef(function HowToPlay({ setIsHow }: Props) {
         // ref={dialog}
       >
         <header id={styles.intro}>
-          <h1>The Simon Game</h1>
-          <h4>How to Play</h4>
+          <Reveal x={-500}>
+            <h1>The Simon Game</h1>
+          </Reveal>
+          <Reveal x={-300}>
+            <h4>How to Play</h4>
+          </Reveal>
         </header>
         <main id={styles.main}>
           <ol>
             {LIST.map((item, idx) => (
-              <li key={idx}>{item.li}</li>
+              <Reveal
+                key={idx}
+                x={-400}
+              >
+                <li>{item.li}</li>
+              </Reveal>
             ))}
           </ol>
         </main>
         <footer id={styles.foot}>
           <form method="dialog">
             <div>
-              <button>Register</button>
-              <button>Login</button>
+              <Reveal x={-500}>
+                <button>Register</button>
+              </Reveal>
+              <Reveal x={-500}>
+                <button>Login</button>
+              </Reveal>
             </div>
             <div>
-              <button onClick={handleClose}>Close</button>
+              <Reveal x={-500}>
+                <button onClick={handleClose}>Close</button>
+              </Reveal>
             </div>
-            <div>
+            <Reveal x={-500}>
               <h4>
                 Tip: <span>Register to save Highscore </span>
               </h4>
-            </div>
+            </Reveal>
           </form>
         </footer>
       </dialog>
