abba39ad75c2c8ea4a77c4cd0df1d469f940a5c2
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-06-14 13:01:45 +0530

    w

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import styles from "./modal.module.scss";
+import Reveal from "./Reveal";
 
 const Form = () => {
   const handleBack = () => {};
@@ -6,20 +8,39 @@ const Form = () => {
   return (
     <>
       <form id={styles.form}>
-        <input
-          type="email"
-          placeholder="Email"
-        ></input>
-        <input
-          type="password"
-          placeholder="Password"
-        ></input>
+        <Reveal>
+          <input
+            type="email"
+            placeholder="Email"
+          ></input>
+        </Reveal>
+        <Reveal>
+          <input
+            type="password"
+            placeholder="Password"
+          ></input>
+        </Reveal>
         <div>
-          <button>Sign Up</button>
-          <button onClick={handleBack}>Go Back</button>;
+          <motion.button
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+          >
+            Sign Up
+          </motion.button>
+          <motion.button
+            initial={{ opacity: 0, x: 50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            onClick={handleBack}
+          >
+            Go Back
+          </motion.button>
         </div>
       </form>
-      <div id={styles.hr} />
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0, transition: { duration: 0.7 } }}
+        id={styles.hr}
+      />
     </>
   );
 };
